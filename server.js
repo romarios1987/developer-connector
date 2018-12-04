9dd3ae93922c2e908356a60ae0efca389f24a0ba
romarios1987 <romios047@gmail.com>
2018-12-04 21:17:34 +0200

    implementing React

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const posts = require('./routes/api/posts');
 const app = express();
 
 // Body parser middleware
-app.use(bodyParser.urlencoded({extend: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 
