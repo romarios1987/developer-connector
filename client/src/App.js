9208f5d734ff126b74e70677f28d6424ab071d79
romarios1987 <romios047@gmail.com>
2018-12-18 23:33:43 +0200

    Creating Login & Register form with state and use React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,35 @@
-import React, { Component } from 'react';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCoffee, faCheckSquare } from '@fortawesome/free-solid-svg-icons'
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faCoffee, faCheckSquare} from '@fortawesome/free-solid-svg-icons'
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 
 import './App.css';
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
 // Usage of Icons Added to Library
 library.add(faCoffee, faCheckSquare);
 
 
 class App extends Component {
-
-  render() {
-    return (
-      <div className="App">
-       <Navbar/>
-        <Landing/>
-        <Footer/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <Navbar/>
+                    <Route exact path="/" component={Landing}/>
+                    <div className="container">
+                        <Route exact path="/register" component={Register}/>
+                        <Route exact path="/login" component={Login}/>
+                    </div>
+                    <Footer/>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
