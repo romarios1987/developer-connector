443fbc06f754cf144833fd0113516f7dcbd411c2
romarios1987 <romios047@gmail.com>
2019-03-26 22:06:35 +0300

    Redux Store & Chrome Extension Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+import {Provider} from 'react-redux';
+import store from './store';
+
+
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {faCoffee, faCheckSquare} from '@fortawesome/free-solid-svg-icons'
+
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
@@ -10,26 +16,29 @@ import './App.css';
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
+
 // Usage of Icons Added to Library
 library.add(faCoffee, faCheckSquare);
 
 
 class App extends Component {
-    render() {
-        return (
+  render() {
+    return (
+          <Provider store={store}>
             <Router>
-                <div className="App">
-                    <Navbar/>
-                    <Route exact path="/" component={Landing}/>
-                    <div className="container">
-                        <Route exact path="/register" component={Register}/>
-                        <Route exact path="/login" component={Login}/>
-                    </div>
-                    <Footer/>
+              <div className="App">
+                <Navbar/>
+                <Route exact path="/" component={Landing}/>
+                <div className="container">
+                  <Route exact path="/register" component={Register}/>
+                  <Route exact path="/login" component={Login}/>
                 </div>
+                <Footer/>
+              </div>
             </Router>
-        );
-    }
+          </Provider>
+    );
+  }
 }
 
 export default App;
